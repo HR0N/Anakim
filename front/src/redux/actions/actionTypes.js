2d9f6c004be003f3cfb6c846bae7f65a4b876a97
Evgeny Kariuk <38400347+Anakimm@users.noreply.github.com>
2021-10-14 18:32:18 +0300

    commit

---
+++ front/src/redux/actions/actionTypes.js
@@ -0,0 +1,11 @@
+/*todo: ......................................................... ......:::::: APP ::::::......     */
+export const TEMPLATE_01 = 'TEMPLATE_01';
+export const TEMPLATE_02 = 'TEMPLATE_02';
+export const TEMPLATE_03 = 'TEMPLATE_03';
+/*todo: ......................................................... ......:::::: HEADER ::::::......     */
+export const DROPDOWN_TOGGLE = 'DROPDOWN_TOGGLE';
+export const HIDE_ALL_DROPDOWN = 'HIDE_ALL_DROPDOWN';
+export const ACCORDION_TOGGLE = 'ACCORDION_TOGGLE';
+/*todo: ......................................................... ......:::::: PLAYER ::::::......     */
+export  const  PLAY_FROM_PLAYLIST = 'PLAY_FROM_PLAYLIST';
+export  const  GET_MUSIC_PLAYLIST = 'GET_MUSIC_PLAYLIST';
